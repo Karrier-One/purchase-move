cae2bc16050793beccb2d1bf3ffac2e955f77dce
Andrew Buchanan <abuchanan@karrier.one>
2024-07-04 13:27:54 -0600

    update typescript

--- ts/event_history.ts
+++ ts/event_history.ts
@@ -1,11 +1,11 @@
-import { getFullnodeUrl, SuiClient, SuiHTTPTransport } from '@mysten/sui.js/client';
+import { getFullnodeUrl, SuiClient } from '@mysten/sui/client';
 const provider = new SuiClient({url: getFullnodeUrl("testnet")});
-const Package = '0x9ee959f9b543aa883a48a9d1739ffbec2530602e7f594ebfa4a5c5c9b000a12b';
-
+const Package = '0x3f45e7e62b25812c7987192208d944adbcc6ad7712ca59d90835e041c9ad8634';
+const OrigPackage = '0x2bd4ae0f9c15728723cfd5a6208108b7c6bfa46622fcc0dd1519fe91d4a709ff';
 async function processEvents() {
   let events = await provider.queryEvents({
       query:{
-        MoveEventType: `${Package}::pay::CoinReceived`
+        MoveEventType: `${OrigPackage}::pay::CoinReceived`
       },
       order: 'descending',
       limit: 1000
@@ -15,7 +15,7 @@ async function processEvents() {
 
   let voucher_events = await provider.queryEvents({
     query:{
-      MoveEventType: `${Package}::pay::KNSVoucherReceived`
+      MoveEventType: `${OrigPackage}::pay::KNSVoucherReceived`
     },
     order: 'descending',
     limit: 1000
--- ts/event_listener.ts
+++ ts/event_listener.ts
@@ -1,16 +1,19 @@
-import { getFullnodeUrl, SuiClient, SuiHTTPTransport } from '@mysten/sui.js/client';
+import { getFullnodeUrl, SuiClient, SuiHTTPTransport } from '@mysten/sui/client';
 import { WebSocket } from 'ws';
  
 
 const provider = new SuiClient({
 	transport: new SuiHTTPTransport({
 		url: getFullnodeUrl('testnet'),
-		WebSocketConstructor: WebSocket as never,
+    websocket: {
+      reconnectTimeout: 1000, // Using 'any' for TypeScript compatibility
+    },
+    WebSocketConstructor: WebSocket as any,
 	}),
 });
-const Package = '0x9ee959f9b543aa883a48a9d1739ffbec2530602e7f594ebfa4a5c5c9b000a12b';
-
-const MoveEventType = '0x9ee959f9b543aa883a48a9d1739ffbec2530602e7f594ebfa4a5c5c9b000a12b::pay::CoinReceived';
+const Package = '0x3f45e7e62b25812c7987192208d944adbcc6ad7712ca59d90835e041c9ad8634';
+const OrigPackage = '0x2bd4ae0f9c15728723cfd5a6208108b7c6bfa46622fcc0dd1519fe91d4a709ff';
+const MoveEventType = '0x2bd4ae0f9c15728723cfd5a6208108b7c6bfa46622fcc0dd1519fe91d4a709ff::pay::CoinReceived';
 let unsubscribe: any = null;
 let Sender: string = '0x2567d98ad32168293b4da76f5a00c1662345181a762895d955a76d19cfb916f6';
 async function processEvents() {
@@ -18,7 +21,7 @@ async function processEvents() {
     unsubscribe = await provider.subscribeEvent({        
         filter: {
           //Sender: Sender
-          MoveEventType: `${Package}::pay::KNSVoucherReceived`
+          MoveEventType: `${OrigPackage}::pay::KNSVoucherReceived`
           //Package: Package,
         },        
         onMessage: async (event:any) => {
